47801b95fbb2fc6dfe9a4fb4c5a53e0973d0ffad
Phil Pursglove <philpursglove@users.noreply.github.com>
2017-06-13 12:18:09 +0100

    Icon setup RFed into separate method

--- X-Wing Visual Builder/X-Wing Visual Builder/MainWindow.xaml.cs
+++ X-Wing Visual Builder/X-Wing Visual Builder/MainWindow.xaml.cs
@@ -19,14 +19,19 @@ namespace X_Wing_Visual_Builder
 
         public MainWindow()
         {
-            string baseLocation = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
-            string filteredLocation = System.IO.Path.GetDirectoryName(baseLocation).Replace("file:\\", "") + "\\Misc\\";
-            Uri iconUri = new Uri(filteredLocation + "RebelIcon.ico");
-            Icon = BitmapFrame.Create(iconUri);
+            SetIcon();
             InitializeComponent();
             ResizeMode = ResizeMode.CanResize;
             WindowState = WindowState.Maximized;
             ComponentDispatcher.ThreadIdle += FillCardCache;
+        }
+
+        private void SetIcon()
+        {
+            string baseLocation = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+            string filteredLocation = System.IO.Path.GetDirectoryName(baseLocation).Replace("file:\\", "") + "\\Misc\\";
+            Uri iconUri = new Uri(filteredLocation + "RebelIcon.ico");
+            Icon = BitmapFrame.Create(iconUri);
         }
 
         void FillCardCache(object sender, EventArgs e)
